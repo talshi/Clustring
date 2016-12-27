64c10f339b914065710be919b02500036d65c4d5
Tal Shiran <talshiran0000@gmail.com>
2016-12-27 07:36:08 +0200

    implement the fetching algorithm

--- clustring.py
+++ clustring.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 import time
 import string
+from sklearn.feature_extraction.text import CountVectorizer
 
 data_file = 'dataset.csv'
 
@@ -11,11 +12,25 @@ def read_data():
     df = pd.read_csv(data_file)
     df = df[['FullDescription']]
     df = pd.DataFrame([doc.lower().translate(None, string.punctuation) for doc in df.FullDescription])
-    return df
 
-def fetch_features():
-    pass
+    data_list = []
+    for v in df.values:
+        data_list.append(str(v))
+    return data_list
+
+
+def extract_features(data):
+    print 'fetching features...'
+    vectorizer = CountVectorizer(max_features=2000)
+    vectorizer.fit_transform(data)
+    # analyze = vectorizer.build_analyzer()
+
+    return vectorizer.get_feature_names()
 
 if __name__ == '__main__':
     data = read_data()
-    print data
+    # print data
+
+    features = extract_features(data)
+    print features
+    print 'amount of features: ', len(features)
