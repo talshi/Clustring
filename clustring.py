388c251757a7435e9dbe22f30c791316640e8edf
Tal Shiran <talshiran0000@gmail.com>
2016-12-27 22:51:07 +0200

    implement pca algorithm

--- clustring.py
+++ clustring.py
@@ -4,8 +4,11 @@ import pandas as pd
 import time
 import string
 from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
 
 data_file = 'dataset.csv'
+
 
 def read_data():
     print 'read data...'
@@ -21,16 +24,53 @@ def read_data():
 
 def extract_features(data):
     print 'fetching features...'
-    vectorizer = CountVectorizer(max_features=2000)
-    vectorizer.fit_transform(data)
-    # analyze = vectorizer.build_analyzer()
+    vectorizer = CountVectorizer(max_features=200)
 
-    return vectorizer.get_feature_names()
+    features_mat = vectorizer.fit_transform(data)
+    features_name =  vectorizer.get_feature_names()
+
+    return features_mat, features_name
+
+
+def pca(data):
+    pca = PCA(n_components=10)
+    pca.fit(data)
+    vars_ = pca.explained_variance_
+
+    plt.plot(range(0, len(vars_)), vars_)
+    plt.ylabel('n_components')
+    plt.show()
+
+    return vars_
+
+def manual_pca(input_mat, num_of_reduced_features=2):
+    cov = np.cov(input_mat.toarray())
+    print cov
+
+    evals, evecs = np.linalg.eigh(cov)
+    print evals
+    print evecs
+
+    idx = np.argsort(evals)[::-1]
+    evecs = evecs[:, idx]
+    evals = evals[idx]
+    evecs = evecs[:, :num_of_reduced_features]
+
+    return np.dot(evecs.T, input_mat.T).T, evals, evecs
+
 
 if __name__ == '__main__':
     data = read_data()
     # print data
 
-    features = extract_features(data)
-    print features
-    print 'amount of features: ', len(features)
+    features_mat, features_name = extract_features(data)
+    print features_name
+    print 'amount of features: ', len(features_name)
+
+    # vars_ = pca(features_mat.toarray())
+    # print vars_
+
+    # vars_ = np.var(features_mat.toarray())
+    # print vars_
+
+    downsampled_data, evals, evecs = manual_pca(input_mat=features_mat, num_of_reduced_features=2)
